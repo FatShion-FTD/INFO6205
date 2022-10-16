7318df99a3cd5e3b508106c5dce7b6ded26d59b7
FatShion-FTD <scyml3@nottingham.edu.cn>
2022-10-16 11:56:00 -0700

    Rotate add method2

--- Quiz1/Rotate.java
+++ Quiz1/Rotate.java
@@ -3,6 +3,7 @@ package Quiz1;
 import java.util.*;
 
 public class Rotate {
+    // Method1
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
@@ -16,6 +17,23 @@ public class Rotate {
         }
         for (int i = k; i < n; i++) {
             nums[i] = remain.get(i - k);
+        }
+    }
+
+    // Method2: O(1) space
+    public void rotate(int[] nums, int k) {      
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int t = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = t;
         }
     }
 }
